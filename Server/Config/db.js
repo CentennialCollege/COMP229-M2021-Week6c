142f8c6921bd477b32a22844cd68e4c968aa6278
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-06-14 16:56:01 -0400

    added db config for MongoDB Atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/clothing_store";
-exports.HostName = "localhost";
+exports.RemoteURI = "mongodb+srv://admin:cd4xEhO1o8mnp6JO@cluster-comp229.1jttx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
